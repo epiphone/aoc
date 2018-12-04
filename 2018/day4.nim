db385ec0614cd19cb4216ae15f5f522eb0e381b8
Aleksi Pekkala <aleksi.v.a.pekkala@student.jyu.fi>
2018-12-04 15:04:23 +0000

    Tidy up the still ugly day 4 solution

--- 2018/day4.nim
+++ 2018/day4.nim
@@ -4,36 +4,16 @@ from strUtils import endsWith, parseInt, split
 import tables
 from times import nil
 
-const TEST_DATA = [
-  "[1518-11-01 00:00] Guard #10 begins shift",
-  "[1518-11-01 00:05] falls asleep",
-  "[1518-11-01 00:25] wakes up",
-  "[1518-11-01 00:30] falls asleep",
-  "[1518-11-01 00:55] wakes up",
-  "[1518-11-01 23:58] Guard #99 begins shift",
-  "[1518-11-02 00:40] falls asleep",
-  "[1518-11-02 00:50] wakes up",
-  "[1518-11-03 00:05] Guard #10 begins shift",
-  "[1518-11-03 00:24] falls asleep",
-  "[1518-11-03 00:29] wakes up",
-  "[1518-11-04 00:02] Guard #99 begins shift",
-  "[1518-11-04 00:36] falls asleep",
-  "[1518-11-04 00:46] wakes up",
-  "[1518-11-05 00:03] Guard #99 begins shift",
-  "[1518-11-05 00:45] falls asleep",
-  "[1518-11-05 00:55] wakes up"
-]
-
-proc step1(): int =
+proc step1() =
   var rows: seq[string]
   for row in lines("day4input.txt"):
     rows.add(row)
   sort(rows, cmp)
 
-  var minutes_asleep_per_guard = newCountTable[string]()
-  var sleepiest_mins = initTable[string, array[60, int]]()
+  var minutes_asleep_per_guard = newCountTable[int]()
+  var sleepiest_mins = initTable[int, array[60, int]]()
   var sleeping_since = times.now()
-  var current_guard: string
+  var current_guard: int
 
   for row in rows:
     var matches: array[2, string]
@@ -41,12 +21,11 @@ proc step1(): int =
     let date = times.parse(matches[0], "yyyy-MM-dd HH:mm")
 
     if matches[1].endsWith("begins shift"):
-      current_guard = split(matches[1])[1]
+      current_guard = parseInt(split(matches[1])[1][1..^1])
     elif matches[1].endsWith("falls asleep"):
       sleeping_since = date
     else: # Woke up
       let slept_minutes = times.between(sleeping_since, date).minutes
-      echo "Guard ", current_guard, " slept for ", slept_minutes
 
       if not minutes_asleep_per_guard.hasKey(current_guard):
         minutes_asleep_per_guard[current_guard] = slept_minutes
@@ -67,14 +46,7 @@ proc step1(): int =
           inc(sleepiest_mins[current_guard][m])
 
   let most_asleep = minutes_asleep_per_guard.largest
-  echo "Most asleep: ", most_asleep[0], " for ", most_asleep[1], " mins"
-
-  var sleepiest_minute = 0
-  for i, mins_asleep in sleepiest_mins[most_asleep[0]]:
-    if mins_asleep > sleepiest_mins[most_asleep[0]][sleepiest_minute]:
-      sleepiest_minute = i
-
-  echo "Sleepiest minute: ", sleepiest_minute
+  echo "The sleepiest guard ", most_asleep[0], " slept in total for ", most_asleep[1], " mins"
 
   # Step 2:
   var
@@ -86,10 +58,10 @@ proc step1(): int =
         max_frequent_guard_id = guard_id
         max_frequent_minute = i
 
-  echo "Most frequent guard: ", max_frequent_guard_id, ", minute: ", max_frequent_minute
-  return 0
+  echo "Most frequent minute was for guard ", max_frequent_guard_id, ", minute ", max_frequent_minute
 
 
 when isMainModule:
   var time = times.cpuTime()
-  echo "Step 1: ", step1(), " took ", (times.cpuTime() - time) * 1000, " ms"
+  step1()
+  echo "Steps 1 and 2 took: ", (times.cpuTime() - time) * 1000, " ms"
