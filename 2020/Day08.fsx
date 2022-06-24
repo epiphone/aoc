d83004bfa06edf1fd76717cb9d309dba82495e12
Aleksi Pekkala <aleksipekkala@gmail.com>
2022-06-24 20:57:47 +0900

    Refactor day 8

--- 2020/Day08.fsx
+++ 2020/Day08.fsx
@@ -2,32 +2,23 @@
 
 open Utils
 
-// Parse input:
-
 type Instruction =
     | Nop of int
     | Acc of int
     | Jmp of int
+
+    static member ofString(str: string) =
+        match str.Split(' ') with
+        | [| "nop"; arg |] -> Nop(int arg)
+        | [| "acc"; arg |] -> Acc(int arg)
+        | [| "jmp"; arg |] -> Jmp(int arg)
+        | _ -> failwith $"Cannot parse instruction {str}"
 
     member this.ToString =
         match this with
         | Nop arg -> $"nop {arg}"
         | Jmp arg -> $"jmp {arg}"
         | Acc arg -> $"acc {arg}"
-
-let parseInstruction (str: string) : Instruction =
-    match str.Split(' ') with
-    | [| "nop"; arg |] -> Nop(int arg)
-    | [| "acc"; arg |] -> Acc(int arg)
-    | [| "jmp"; arg |] -> Jmp(int arg)
-    | _ -> failwith $"Cannot parse instruction {str}"
-
-let instructions =
-    "./input08.txt"
-    |> System.IO.File.ReadAllLines
-    |> Array.map parseInstruction
-
-// Step 1:
 
 type Computer =
     { Instructions: Instruction []
@@ -39,22 +30,32 @@ type Computer =
           Acc = 0
           Index = 0 }
 
-let tick (cmp: Computer) : Computer =
-    match cmp.Instructions[cmp.Index] with
-    | Nop _ -> { cmp with Index = cmp.Index + 1 }
-    | Acc arg ->
-        { cmp with
-            Index = cmp.Index + 1
-            Acc = cmp.Acc + arg }
-    | Jmp arg -> { cmp with Index = cmp.Index + arg }
+    member this.Tick: Computer =
+        match this.Instructions[this.Index] with
+        | Nop _ -> { this with Index = this.Index + 1 }
+        | Acc arg ->
+            { this with
+                Index = this.Index + 1
+                Acc = this.Acc + arg }
+        | Jmp arg -> { this with Index = this.Index + arg }
+
+    /// jepa
+    member this.Terminating: bool = this.Index = this.Instructions.Length - 1
+
+let instructions =
+    "./input08.txt"
+    |> System.IO.File.ReadAllLines
+    |> Array.map Instruction.ofString
+
+// Step 1:
 
 let tickUntilLoop (cmp: Computer) : Computer =
-    let rec loop currCmp visitedIndexes =
-        match tick currCmp with
-        | cmp when Set.contains cmp.Index visitedIndexes -> cmp
-        | cmp -> loop cmp (Set.add cmp.Index visitedIndexes)
+    let rec loop (currCmp: Computer) (visitedIndexes: Set<int>) =
+        match currCmp.Tick with
+        | nextCmp when visitedIndexes.Contains nextCmp.Index -> nextCmp
+        | nextCmp -> loop nextCmp (visitedIndexes.Add nextCmp.Index)
 
-    loop cmp (set [])
+    loop cmp Set.empty
 
 let step1 () =
     instructions
@@ -64,16 +65,23 @@ let step1 () =
 
 // Step 2:
 
-let terminated (cmp: Computer) : bool = cmp.Index = cmp.Instructions.Length - 1
-
 let tickUntilLoopOrTerminate (cmp: Computer) : Computer =
-    let rec loop currCmp visitedIndexes =
-        match tick currCmp with
-        | cmp when terminated cmp -> cmp
-        | cmp when Set.contains cmp.Index visitedIndexes -> cmp
-        | cmp -> loop cmp (Set.add cmp.Index visitedIndexes)
+    let rec loop (currCmp: Computer) (visitedIndexes: Set<int>) =
+        match currCmp.Tick with
+        | nextCmp when nextCmp.Terminating -> nextCmp
+        | nextCmp when visitedIndexes.Contains nextCmp.Index -> nextCmp
+        | nextCmp -> loop nextCmp (visitedIndexes.Add nextCmp.Index)
 
-    loop cmp (set [])
+    loop cmp Set.empty
+
+let tickUntilTerminating (cmp: Computer) : option<Computer> =
+    let rec loop (currCmp: Computer) (visitedIndexes: Set<int>) =
+        match currCmp.Tick with
+        | nextCmp when nextCmp.Terminating -> Some nextCmp
+        | nextCmp when visitedIndexes.Contains nextCmp.Index -> None
+        | nextCmp -> loop nextCmp (visitedIndexes.Add nextCmp.Index)
+
+    loop cmp Set.empty
 
 let instructionPermutations (instructions: Instruction []) : seq<Instruction []> =
     seq {
@@ -86,24 +94,21 @@ let instructionPermutations (instructions: Instruction []) : seq<Instruction []>
 
             if Option.isSome changedInstruction then
                 let originalInstruction = instructions[i]
-                Array.set instructions i (Option.get changedInstruction)
+                instructions.SetValue(changedInstruction.Value, i)
                 yield instructions
-                Array.set instructions i originalInstruction
+                instructions.SetValue(originalInstruction, i)
     }
 
-let tweakInstructionsUntilTerminate (instructions: Instruction []) : Computer =
+let tweakInstructionsUntilTerminate (instructions: Instruction []) =
     instructions
     |> instructionPermutations
-    |> Seq.map (fun insts ->
-        insts
-        |> Computer.Default
-        |> tickUntilLoopOrTerminate)
-    |> Seq.find terminated
+    |> Seq.map (fun insts -> insts |> Computer.Default |> tickUntilTerminating)
+    |> Seq.pick id
 
 let step2 () =
     instructions
     |> tweakInstructionsUntilTerminate
-    |> (fun cmp -> (tick cmp).Acc)
+    |> (fun cmp -> cmp.Tick.Acc)
 
 benchmark [| ("step1", step1)
              ("step2", step2) |]
